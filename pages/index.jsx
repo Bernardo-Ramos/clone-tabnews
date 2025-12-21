9c3c8f4732d3b3b579bd48dbf33583c7e4dd6ec8
Bernardo-Ramos <65903782+Bernardo-Ramos@users.noreply.github.com>
2025-12-21 11:32:28 +0000

    adicionada nova frase

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ function Home() {
     return (
         <>
             <h1>MUITO GUEI 🌈</h1>
+            <h1>Nova mensagem</h1>
             <img 
                 src='https://media1.tenor.com/m/FdKKZB5Oc9UAAAAd/freaky-sonic-rubbing-hands.gif' 
                 alt='freaky-sonic'
