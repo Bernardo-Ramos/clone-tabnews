40211f49270249bf498fde45f4513cf3aa51b85a
Bernardo-Ramos <65903782+Bernardo-Ramos@users.noreply.github.com>
2025-12-24 13:37:43 +0000

    adiciona dependencia dev , script  e script

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,16 @@
 function Home() {
-    return (
-        <>
-            <h1>MUITO GUEI 🌈</h1>
-            <h1>Nova mensagem</h1>
-            <img 
-                src='https://media1.tenor.com/m/FdKKZB5Oc9UAAAAd/freaky-sonic-rubbing-hands.gif' 
-                alt='freaky-sonic'
-                width={300}
-                height={300}
-            />
-        </>
-    );
+  return (
+    <>
+      <h1>MUITO GUEI 🌈</h1>
+      <h1>Nova mensagem</h1>
+      <img
+        src="https://media1.tenor.com/m/FdKKZB5Oc9UAAAAd/freaky-sonic-rubbing-hands.gif"
+        alt="freaky-sonic"
+        width={300}
+        height={300}
+      />
+    </>
+  );
 }
 
 export default Home;
